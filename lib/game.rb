9fb2c6626686a05ace2c44b4934096d58e7d73a3
deniselupe <dgrodriguez24@hotmail.com>
2022-07-22 00:12:28 +0000

    feat: add player instances to game instance

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+require_relative 'player'
+
 # This is the game class that defines the behaviors and attributes for Tic-Tac-Toe
 class Game
+  attr_reader :player_list
+
+  def initialize(players: [])
+    @player_list = players
+  end
+
+  def game_start
+    add_players
+  end
+
+  def add_players
+    2.times { @player_list << Player.new }
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,24 @@
 require_relative '../lib/game.rb'
 
 describe Game do
+  describe '#add_players' do
+    subject(:game1) { described_class.new }
+
+    context 'when adding players' do
+      it '@player_list receives :<< twice' do
+        expect(game1.player_list).to receive(:<<).twice
+        game1.add_players
+      end
+
+      it 'changes length of @player_list by two' do
+        expect { game1.add_players }.to change { game1.player_list.length }.from(0).to(2)
+      end
+
+      it 'Player instances append to @player_list' do
+        game1.add_players
+        players = game1.player_list
+        expect(players).to all(be_an_instance_of(Player))
+      end
+    end
+  end
 end
